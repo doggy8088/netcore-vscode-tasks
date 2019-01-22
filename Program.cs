9e3fb358d6081ac7cde5d0d7d1cabb2d34ecaeb4
Will <doggy.huang@gmail.com>
2019-01-22 21:49:19 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcore_vscode_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
